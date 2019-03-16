5f895c6c31af9004a2c4768ac067da4f65170a11
Joe Aguilar <jose.aguilar.6694@gmail.com>
2019-03-16 15:26:14 -0700

    Initial Commit

---
+++ PassTests/PassTests.swift
@@ -0,0 +1,34 @@
+//
+//  PassTests.swift
+//  PassTests
+//
+//  Created by Jose Aguilar on 3/16/19.
+//  Copyright © 2019 Jose Aguilar. All rights reserved.
+//
+
+import XCTest
+@testable import Pass
+
+class PassTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
