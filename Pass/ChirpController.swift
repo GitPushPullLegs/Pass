fb4308cb96da2b576ea1b68a2b9f780b1fef6539
Jose Aguilar <jose.aguilar.6694@gmail.com>
2019-04-24 21:07:14 -0700

    Created sendChirp function within ChirpController and added microphone privacy description in Info.plist.

--- Pass/ChirpController.swift
+++ Pass/ChirpController.swift
@@ -11,5 +11,34 @@ import ChirpConnect
 
 class ChirpController: NSObject {
 
-    
+    static let shared = ChirpController()
+
+    let chirp = ChirpConnect(appKey: SecretsHandler.chirpAppKey, andSecret: SecretsHandler.chirpAppSecret)
+
+    private override init() {
+        super.init()
+
+        if let error = chirp?.setConfig(SecretsHandler.chirpAppConfig) {
+            print("Error: \(error)")
+        } else {
+            if let error = chirp?.start() {
+                print("Error: \(error)")
+            } else {
+                print("Chirp enabled.")
+            }
+        }
+    }
+
+    func sendChirp(withMessage message: String, completionHandler: ((Bool, Error?) -> Void)) {
+        guard let payload = message.data(using: .utf8) else {
+            completionHandler(false, nil)
+            return
+        }
+        if let error = chirp?.send(payload) {
+            completionHandler(false, error)
+            
+        } else {
+            completionHandler(true, nil)
+        }
+    }
 }
