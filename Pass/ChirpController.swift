fd505015375dccaf81325dfce5484c2f8832f5a4
Jose Aguilar <jose.aguilar.6694@gmail.com>
2019-04-24 20:51:30 -0700

    Configuring Chirp

---
+++ Pass/ChirpController.swift
@@ -0,0 +1,15 @@
+//
+//  ChirpController.swift
+//  Pass
+//
+//  Created by Jose Aguilar on 4/24/19.
+//  Copyright © 2019 Jose Aguilar. All rights reserved.
+//
+
+import UIKit
+import ChirpConnect
+
+class ChirpController: NSObject {
+
+    
+}
